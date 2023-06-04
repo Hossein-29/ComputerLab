bfaab12afb424abb6f5c527ffa2a0f3e77ac171d
Abolfazl <117838887+abolfazl2004shd@users.noreply.github.com>
2023-06-04 12:29:13 +0330

    create loop for Print

--- help.cs
+++ help.cs
@@ -5,6 +5,9 @@ namespace Name
     Class P {
         static public class Main() {
             Console.WriteLine("hello hossein :D");
+            for(int i =0; i < 10; i++){
+            Console.WriteLine(i);
+            }
         }
     }
 }
