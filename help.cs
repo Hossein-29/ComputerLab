0192db50e60a91b12c516fb2b7e8c6af282d3334
Abolfazl Shahidi <shahidi2004shahidi@gmail.com>
2023-06-04 23:07:33 +0330

    car

--- help.cs
+++ help.cs
@@ -1,10 +1,6 @@
 using System;
 namespace Name
 {
-    Class Person {
-        public Int Id;
-        public String Name;
-    }
     Class Car {
         public Int CarId;
         public String CarName;
