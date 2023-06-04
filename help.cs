1018d6b0178762e442b92ce20ece3e0003e929e6
Abolfazl Shahidi <shahidi2004shahidi@gmail.com>
2023-06-04 23:03:21 +0330

    person class

--- help.cs
+++ help.cs
@@ -1,18 +1,17 @@
 using System;
 namespace Name
 {
+    Class Person {
+        public Int Id;
+        public String Name;
+    }
+    
     Class P
     {
         static public class Main() {
-            Console.WriteLine("hello hossein :D");
-            for(int i =0; i< 10; i++){
-            Console.WriteLine(i);
-
             
-}
-int n = 56;
-Console.WriteLine("noooooooooooooooooooooooooo");
-List<int> values = new List<int>();
+
+
         }
     }
 }
